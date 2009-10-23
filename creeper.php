eb0c4fd4c62ef1c110df671ff07da5da5246dcc2
houdw <houdw@shanghai-air.com>
2009-10-23 16:50:15 +0800

    增加了sqlite2数据库支持 creeper支持本地磁盘文件缓存

--- creeper.php
+++ creeper.php
@@ -1,13 +1,96 @@
 <?php  
+	require_once 'clue/database.php';
+	require_once 'clue/tool.php';
+	
+	class Clue_Creeper_Cache{
+		private $cache;
+		private $db;
+		private $base36;
+		
+		function __construct($cache){
+			$this->base36=new Clue_Base36();
+			
+			if(is_dir($cache) && file_exists($cache)){
+				$this->cache=$cache;
+				$this->db=Clue_Database::create('sqlite', array('db'=>"$cache/cache.db"));
+				
+				// check if cache table exists
+				if(!$this->db->has_table('cache')){
+					$this->db->exec("
+						CREATE TABLE [cache] (
+							[id] INTEGER  NOT NULL PRIMARY KEY,
+							[url] VARCHAR(4096)  UNIQUE NULL,
+							[last_fetch] INTEGER  NULL,
+							[ttl] INTEGER DEFAULT '86400' NULL
+						)
+					");
+				}
+			}
+		}
+		
+		function cache_file($id, $writing=false){
+			$enc=$this->base36->encode($id);
+			$folder=strlen($enc)>=2 ? "$this->cache/".substr($enc, 0, 2) : "$this->cache/00";
+			$file="$folder/$enc";
+			
+			if($writing && !file_exists($folder)) mkdir($folder);
+			
+			return $file;
+		}
+		
+		function get($url){
+			// TODO: check TTL against last_fetch
+			
+			$url=$this->db->quote($url);
+			$id=$this->db->get_var("select id from cache where url=$url");
+			if($id){
+				//echo "HIT($id): $url\n";
+				// Try to load content from cache file
+				$file=$this->cache_file($id);
+				if(file_exists($file)){
+					return file_get_contents($file);
+				}
+				else{
+					// Cache file missing, delete record in database.
+					$this->db->exec("delete from cache where id=$id");
+					echo "Cache file missing, record removed($id)\n";
+				}
+			}
+		}
+		
+		function put($url, $content){
+			$url=$this->db->quote($url);
+			$last_fetch=time();
+			
+			$this->db->exec("insert into cache(url, last_fetch) values($url, $last_fetch)");
+			$id=$this->db->insertId();
+			
+			//echo "Caching $url to $id ";
+			$file=$this->cache_file($id, true);
+			file_put_contents($file, $content);
+			//echo "\n";
+		}
+	}
+	
 	class Clue_Creeper{
 		public $response;
 		public $content;
+		private $cache;
+		private $cachedb;
 		
 		private $curl;
 		
-		function __construct(){
+		function __construct($option=array()){
+			if(isset($option['cache'])) $this->cache=new Clue_Creeper_Cache($option['cache']);
+			
 			$this->curl=curl_init();
 			curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+			
+			if(isset($option['proxy'])){
+				list($proxy, $port)=explode(":", $option['proxy']);
+				curl_setopt($this->curl, CURLOPT_PROXY, $proxy);
+				curl_setopt($this->curl, CURLOPT_PROXYPORT, $port);
+			}
 		}
 		
 		function __destruct(){
@@ -15,8 +98,15 @@
 		}
 		
 		function open($url){
-			curl_setopt($this->curl, CURLOPT_URL, $url);
-			$this->content=curl_exec($this->curl);
+			// check cache
+			$this->content= $this->cache ? $this->cache->get($url) : false;
+			if(!$this->content){				
+				// echo "Creeping $url\n";
+				curl_setopt($this->curl, CURLOPT_URL, $url);
+				$this->content=curl_exec($this->curl);
+				
+				if($this->cache) $this->cache->put($url, $this->content);	// save cache
+			}
 		}
 	}
 	
