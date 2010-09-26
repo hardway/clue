dd2d462a0a4057785d86b33e40a98857bab31fa0
houdw <houdw@shanghai-air.com>
2010-09-26 12:10:43 +0800

    增加CVS读取

---
+++ text/csv.php
@@ -0,0 +1,29 @@
+<?php  
+    class Clue_Text_CSV{
+        public $filename;
+        
+        public $columns;
+        public $rows;
+        
+        function __construct($filename){
+            $this->columns=array();
+            $this->rows=array();
+            
+            $this->filename=$filename;
+        }
+        
+        function read(){
+            if(file_exists($this->filename)){
+                $f=fopen($this->filename, "r");
+                
+                $this->columns=fgetcsv($f);
+                while(!feof($f)){
+                    $r=fgetcsv($f);
+                    if(is_array($r)) $this->rows[]=$r;
+                }
+                
+                fclose($f);
+            }
+        }
+    }
+?>
