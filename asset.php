c620f2a5ac145e65f901dfa9541a45860e6c63fa
Hardway Hou <hardway.hou@gmail.com>
2012-07-03 14:11:35 +0800

    修改clue\asset

--- asset.php
+++ asset.php
@@ -1,33 +1,56 @@
 <?php
 namespace Clue{
-	class Clue_Asset{
-		static public $ASSET_BASE=".";
-		
-		public $name;
+	class Asset{
 		public $type;
-		public $paths;
+		public $files;
 		
-		function __construct($name, $paths){
-			$this->name=$name;
-			$this->type=substr($name, strrpos($name, ".")+1);
-			$this->paths=$paths;
+		function __construct($files){
+			$this->files=array();
+			$this->add($files);
+		}
+
+		function add($files){
+			if(!is_array($files)) $files=array($files);
+
+			while($f=array_shift($files)){
+				if(strpos($f, "*")!==false){
+					foreach(array_reverse(glob($f)) as $_){
+						array_unshift($files, $_);
+					}
+					continue;
+				}
+
+				if(!in_array($f, $this->files)){
+					$this->files[]=$f;
+				}				
+			}
 		}
 		
-		function compile($output=null){
+		function compile(){
+			$ext=array();
+
 			// Combine asset file contents
 			$content="";
-			foreach($this->paths as $path){
-				$content.=@file_get_contents(self::$ASSET_BASE."/".$path);
+			foreach($this->files as $f){
+				$pi=pathinfo($f);
+				@$ext[$pi['extension']]++;
+
+				$content.=@file_get_contents($f);
+			}
+
+			if(count($ext)==1){
+				$this->type=array_keys($ext);
+				$this->type=$this->type[0];
 			}
 			
-			if($output){
-				//TODO: save content to file
-			}
-			else
-				return $content;
+			// TODO: cache
+
+			return $content;
 		}
 		
-		function render(){
+		function dump(){
+			$content=$this->compile();
+
 			// Output contents according to asset type
 			header("Cache-Control: must-revalidate");
 			switch($this->type){
@@ -41,8 +64,6 @@ namespace Clue{
 					header("Content-Type: text/plain");
 					break;
 			}
-			
-			$content=$this->compile();
 			
 			header('Content-Length: '.strlen($content));
 			echo $content;
