d616fc1855d37622a99a807e194fc019e04417ea
Hardway Hou <hardway.hou@gmail.com>
2013-02-01 08:53:20 +0800

    Clue\mail using PHPMailer

--- mail.php
+++ mail.php
@@ -1,74 +1,46 @@
 <?php  
     namespace Clue;
-    class Mail{
-        public $subject;
-        public $body;
-        public $header;
-        public $from;
-        public $recipient;
-        protected $attach;
-        protected $boundary;
-        
-        function __construct(){
-            $this->header=array();
-            $this->attach=array();
-            $this->boundary="<<CLUE-".md5(time()).">>";
-        }
-        
-        function attach($path, $name=null){
-            if(!is_file($path)) return false;
-            
-            if(empty($name)) $name=basename($path);
-            $type='application/octet-stream';
-            
-            $this->attach[]="Content-Type:$type name=\"$name\"";
-            $this->attach[]= "Content-Transfer-Encoding: base64"; 
-            $this->attach[]= "Content-Disposition: attachment; filename=\"$name\"\r\n"; 
-            $this->attach[]= chunk_split(base64_encode(file_get_contents($path))); 
-            $this->attach[]="--".$this->boundary;
-        }
-        
-        function send(){
-            // TODO: plain text email
-            $recipient=is_array($this->recipient) ? implode(", ", $this->recipient) : $this->recipient;
+    class Mail{   
+        public $mailer;
 
-            if(!empty($reply)) $mail->header[]="From: $reply";
-            
-            $header =is_array($this->header) ? implode("\r\n", $this->header) : $this->header;
-            $header.="\r\nMIME-Version: 1.0\r\nContent-Type: multipart/mixed; boundary=\"".$this->boundary."\"\r\n";
-            
-            $message ="--$this->boundary\r\n";
-            $message.="Content-Type: text/html; charset=\"UTF-8\"\r\n";
-            $message.="Content-Transfer-Encoding: quoted-printable\r\n\r\n";            
-            $message.=quoted_printable_encode($this->body);
-            $message.="\r\n--$this->boundary\r\n";
-            $message.=implode("\r\n", $this->attach);
-            
-            // TODO: error check and logging
-            $ret=mail($recipient, $this->subject, $message, $header);
-            
-            return $ret;
-        }
+        function __construct($options){
+            $this->options=array_merge(array(
+                'port'=>25,
+                'charset'=>'UTF-8'
+            ), $options);
 
-        function send_to($recipients, $undisclosed=true){        
-            $this->recipient=is_array($recipients) ? $recipients : array($recipients);
-            if($undisclosed){
-                $this->header[]='To: Undisclosed Recipients';
-                $this->header[]='BCC: '.implode(', ', $this->recipient);
-                $this->recipient=null;
+            $this->mailer=new \PHPMailer();
+            $this->mailer->IsSMTP();
+            $this->mailer->Host=$this->options['host'];
+            $this->mailer->Port=$this->options['port'];
+            $this->mailer->CharSet=$this->options['charset'];
+
+            if(!empty($this->options['username'])){
+                $this->mailer->SMTPAuth=true;
+                $this->mailer->Username=$this->options['username'];
+                $this->mailer->Password=$this->options['password'];
             }
-            $this->send();
+            if(!empty($this->options['secure'])){
+                $this->mailer->SMTPSecure=$this->options['secure'];
+            }
         }
-        
-        /* Deprecated */
-        static function broadcast($recipient, $subject, $body, $reply='noreply', $undisclosed=true){
-            $mail=new Clue_Mail();
+                
+        function send($subject, $html, $to, $from=null, $reply=null){
+            $this->mailer->Subject=$subject;
+            $this->mailer->MsgHTML($html);
             
-            $mail->from=$reply;
-            $mail->subject=$subject;
-            $mail->body=$body;
-            
-            return $mail->send_to($recipient, $undisclosed);
+            if(!empty($from)){
+                $this->mailer->SetFrom($from);
+            }
+
+            if(!empty($reply))
+                $this->mailer->AddReplyTo($reply);
+
+            $this->mailer->AddAddress($to);
+
+            $ret=$this->mailer->Send();
+
+            return $ret;
         }
     }
 ?>
