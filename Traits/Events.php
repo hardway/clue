19285f401ca2657ae234179260a255fdda9906b9
Hardway Hou <hardway.hou@gmail.com>
2015-03-10 09:53:27 +0800

    Replace Observer with Events trait

--- Traits/Events.php
+++ Traits/Events.php
@@ -1,9 +1,9 @@
 <?php
 namespace Clue\Traits;
 
-trait Observer{
-    function notify($event, $data=null){
-        // Observer always listen on itself by default
+trait Events{
+    function fire_event($event, $data=null){
+        // Always listen on itself by default
         if(method_exists($this, "on_$event")){
             call_user_func([$this, "on_$event"], $this, $data);
         }
@@ -11,7 +11,10 @@ trait Observer{
         if(is_array(@$this->_events)) foreach($this->_events as $name=>$listeners){
             if(preg_match('/'.$name.'/', $event)){
                 if(is_array($listeners)) foreach($listeners as $l){
-                    if(method_exists($l, "on_$event")){
+                    if(is_callable($l)){
+                        call_user_func($l, $this, $data);
+                    }
+                    elseif(method_exists($l, "on_$event")){
                         call_user_func([$l, "on_$event"], $this, $data);
                     }
                 }
@@ -19,7 +22,7 @@ trait Observer{
         }
     }
 
-    function attach($listener, $events){
+    function add_event_listener($events, $listener){
         $events=is_string($events) ? [$events] : $events;
         foreach($events as $e){
             if(!isset($this->_events[$e])) $this->_events[$e]=[];
@@ -27,7 +30,7 @@ trait Observer{
         }
     }
 
-    function detach($listener, $events=null){
+    function remove_event_listener($events, $listener=null){
         if($events==null){
             $events=array_keys(@$this->_events) ?: [];
         }
