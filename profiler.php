1f90b597f8bbf18cafae016669afc14079f0d650
Hardway Hou <hardway.hou@gmail.com>
2015-07-21 23:18:27 +0800

    xhprof with request data

--- profiler.php
+++ profiler.php
@@ -8,7 +8,7 @@
 namespace Clue;
 
 class Profiler{
-    function __construct($source='unknown'){
+    function __construct($source='xhprof'){
     	$this->xhprof=extension_loaded("xhprof");
 
     	$this->start($source);
@@ -17,10 +17,10 @@ class Profiler{
     }
 
     function __destruct(){
-
+        $this->stop();
     }
 
-    function start($source='unknown'){
+    function start($source='xhprof'){
     	$this->start_native();
 
 	    if($this->xhprof) $this->start_xhprof($source);
@@ -44,13 +44,15 @@ class Profiler{
     	$this->stop_memory=memory_get_usage();
     }
 
-    function start_xhprof($source='unknown'){
+    function start_xhprof($source='xhprof'){
 		\xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
 
-        $this->xhprof_run=date("mdHis").uniqid();
+        $this->xhprof_run=date("Ymd_His_").uniqid();
         $this->xhprof_source=$source;
         $this->xhprof_dir=ini_get("xhprof.output_dir");
         $this->xhprof_file=sprintf("%s/%s.%s.xhprof", $this->xhprof_dir, $this->xhprof_run, $this->xhprof_source);
+        $this->xhprof_request=sprintf("%s/%s.%s.request", $this->xhprof_dir, $this->xhprof_run, $this->xhprof_source);
+        file_put_contents($this->xhprof_request, serialize(['SERVER'=>$_SERVER, 'POST'=>$_POST, 'GET'=>$_GET]));
 
         $om=umask(0);
         if(!is_dir($this->xhprof_dir)) @mkdir($this->xhprof_dir, 0777, true);
@@ -60,7 +62,12 @@ class Profiler{
     function stop_xhprof(){
         $data = \xhprof_disable();
 
-		file_put_contents($this->xhprof_file, serialize($data));
+        if(!empty($data)){
+            file_put_contents($this->xhprof_file, serialize($data));
+        }
+        else{
+            var_dump($data);
+        }
     }
 
     function summary(){
@@ -68,10 +75,10 @@ class Profiler{
     		'time'=>$this->stop_time - $this->start_time,
     		'memory'=>$this->stop_memory - $this->start_memory,
     		'xhprof'=>$this->xhprof ? [
-                        'run'       => $this->xhprof_run,
-                        'source'    => $this->xhprof_source,
-                        'url'       => "http://localhost/xhprof/xhprof_html/index.php?run=$this->xhprof_run&source=".$this->xhprof_source
-                    ]: null
+                'run'       => $this->xhprof_run,
+                'source'    => $this->xhprof_source,
+                'url'       => "http://localhost/xhprof/xhprof_html/index.php?run=$this->xhprof_run&source=".$this->xhprof_source
+            ]: null
     	);
     }
 }
