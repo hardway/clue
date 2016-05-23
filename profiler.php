6017ba05a885e44b66fd850442a0717824b0475b
Hardway Hou <hardway.hou@gmail.com>
2016-05-23 12:42:01 +0800

    bugfix: 文件名大小写

--- profiler.php
+++ profiler.php
@@ -8,76 +8,88 @@
 namespace Clue;
 
 class Profiler{
-    function __construct($source='xhprof', $options=[]){
-    	$this->xhprof=extension_loaded("xhprof");
-        $this->xhprof_options=$options;
+	function __construct($source='xhprof', $options=[]){
+		$this->profiler=@$options['profiler'] ?: 'xhprof';
 
-    	$this->start($source);
+		$this->xhprof=extension_loaded($this->profiler);
+		$this->xhprof_options=$options;
 
-        register_shutdown_function(array($this, 'stop'));
-    }
+		$this->start($source);
 
-    function __destruct(){
-        $this->stop();
-    }
+		register_shutdown_function(array($this, 'stop'));
+	}
 
-    function start($source='xhprof'){
-    	$this->start_native();
+	function __destruct(){
+		$this->stop();
+	}
 
-	    if($this->xhprof) $this->start_xhprof($source);
-    }
+	function start($source='xhprof'){
+		$this->start_native();
 
-    function stop(){
-        $this->stop_native();
+		if($this->xhprof) $this->start_xhprof($source);
+	}
 
-    	if($this->xhprof) $this->stop_xhprof();
+	function stop(){
+		$this->stop_native();
 
-        return $this->summary();
-    }
+		if($this->xhprof) $this->stop_xhprof();
 
-    function start_native(){
-	    $this->start_time=microtime(true);
-	    $this->start_memory=memory_get_usage();
-    }
+		return $this->summary();
+	}
 
-    function stop_native(){
-    	$this->stop_time=microtime(true);
-    	$this->stop_memory=memory_get_usage();
-    }
+	function start_native(){
+		$this->start_time=microtime(true);
+		$this->previous_time=$this->start_time;
 
-    function start_xhprof($source='xhprof'){
-		\xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY, $this->xhprof_options);
+		$this->start_memory=memory_get_usage();
+	}
 
-        $this->xhprof_run=date("Ymd_His_").uniqid();
-        $this->xhprof_source=$source;
-        $this->xhprof_dir=ini_get("xhprof.output_dir");
-        $this->xhprof_file=sprintf("%s/%s.%s.xhprof", $this->xhprof_dir, $this->xhprof_run, $this->xhprof_source);
-        $this->xhprof_request=sprintf("%s/%s.%s.request", $this->xhprof_dir, $this->xhprof_run, $this->xhprof_source);
-        file_put_contents($this->xhprof_request, serialize(['SERVER'=>$_SERVER, 'POST'=>$_POST, 'GET'=>$_GET]));
+	function stop_native(){
+		$this->stop_time=microtime(true);
+		$this->stop_memory=memory_get_usage();
+	}
 
-        $om=umask(0);
-        if(!is_dir($this->xhprof_dir)) @mkdir($this->xhprof_dir, 0777, true);
-        umask($om);
-    }
+	function start_xhprof($source='xhprof'){
+		$func=$this->profiler.'_enable';
+		$func(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY, $this->xhprof_options);
 
-    function stop_xhprof(){
-        $data = \xhprof_disable();
+		$this->xhprof_run=date("Ymd_His_").uniqid();
+		$this->xhprof_source=$source;
+		$this->xhprof_dir=ini_get("xhprof.output_dir");
+		$this->xhprof_file=sprintf("%s/%s.%s.xhprof", $this->xhprof_dir, $this->xhprof_run, $this->xhprof_source);
+		$this->xhprof_request=sprintf("%s/%s.%s.request", $this->xhprof_dir, $this->xhprof_run, $this->xhprof_source);
+		file_put_contents($this->xhprof_request, serialize(['SERVER'=>$_SERVER, 'POST'=>$_POST, 'GET'=>$_GET]));
 
-        if(!empty($data)){
-            file_put_contents($this->xhprof_file, serialize($data));
-        }
-    }
+		$om=umask(0);
+		if(!is_dir($this->xhprof_dir)) @mkdir($this->xhprof_dir, 0777, true);
+		umask($om);
+	}
 
-    function summary(){
-    	return array(
-    		'time'=>$this->stop_time - $this->start_time,
-    		'memory'=>$this->stop_memory - $this->start_memory,
-    		'xhprof'=>$this->xhprof ? [
-                'run'       => $this->xhprof_run,
-                'source'    => $this->xhprof_source,
-                'url'       => "http://localhost/xhprof/xhprof_html/index.php?run=$this->xhprof_run&source=".$this->xhprof_source
-            ]: null
-    	);
-    }
+	function stop_xhprof(){
+		$func=$func=$this->profiler.'_disable';
+		$data = $func();
+
+		if(!empty($data)){
+			file_put_contents($this->xhprof_file, serialize($data));
+		}
+	}
+
+	function ticktock($msg="Tick Tock"){
+		$now=microtime(true);
+		error_log(sprintf("%f (%f): %s", $now - $this->start_time, $now - $this->previous_time?:$this->start_time, $msg));
+		$this->previous_time=$now;
+	}
+
+	function summary(){
+		return array(
+			'time'=>$this->stop_time - $this->start_time,
+			'memory'=>$this->stop_memory - $this->start_memory,
+			'xhprof'=>$this->xhprof ? [
+				'run'       => $this->xhprof_run,
+				'source'    => $this->xhprof_source,
+				'url'       => "http://localhost/xhprof/xhprof_html/index.php?run=$this->xhprof_run&source=".$this->xhprof_source
+			]: null
+		);
+	}
 }
 ?>
