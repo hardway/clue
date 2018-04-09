6b14d04c0d50dbaaf0883f19d8cc431db60cb0d8
Hardway Hou <hardway.hou@gmail.com>
2018-04-09 18:15:02 +0800

    minor: text

--- database/mongodb.php
+++ database/mongodb.php
@@ -7,7 +7,7 @@ namespace Clue\Database{
         protected $_result;
 
         function __construct(array $param){
-            if(!extension_loaded('mongodb')) throw new \Exception(__CLASS__.": extension mongo is missing!");
+            if(!extension_loaded('mongodb')) throw new \Exception(__CLASS__.": extension mongodb is missing!");
 
             $conn_str=sprintf("mongodb://%s:%s", @$param['host']?:'localhost', @$param['port']?:27017);
             $this->conn=new \MongoDB\Driver\Manager($conn_str);
