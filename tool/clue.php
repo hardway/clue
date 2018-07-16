c3ab1fdf20ce02395eaeea46f6b85c956076aed5
Hardway Hou <hardway.hou@gmail.com>
2018-07-16 17:31:29 +0800

    minor: adjust cli(clue rpc) parameter

--- tool/clue.php
+++ tool/clue.php
@@ -379,20 +379,20 @@ END
     /**
      * RPC测试
      * @param $endpoint 服务端
-     * @param $client 用户
-     * @param $token 密钥
      * @param $function 调用函数
      * @param $params 参数（如果是文件名，则加载其中的JSON数据）
+     * @param $client 用户
+     * @param $token 密钥
      */
-    function clue_rpc($endpoint, $client, $token, $function, $params){
+    function clue_rpc($endpoint, $function, $params="[]", $client=null, $token=null){
         $c=new Clue\RPC\Client($endpoint, array('debug'=>true, 'client'=>$client, 'token'=>$token));
         $c->enable_log();
 
         if(is_string($params) && file_exists($params)){
-            $params=json_decode(file_get_contents($params), true);
+            $params=file_get_contents($params);
         }
 
-        $ret=call_user_func_array(array($c, $function), $params);
+        $ret=call_user_func_array(array($c, $function), json_decode($params, true));
 
         print_r($ret);
     }
