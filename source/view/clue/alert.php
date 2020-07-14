92dc711096632310a07c3357adcb759840c5c00b
Hardway Hou <hardway.hou@gmail.com>
2020-07-14 11:02:52 +0800

    minor: alert view

---
+++ source/view/clue/alert.php
@@ -0,0 +1,4 @@
+<div class='toast toast-<?=$level?>'>
+    <button type='button' class='btn btn-clear float-right' onclick='$(this).getParent(".toast").destroy();'></button>
+    <?=$message?>
+</div>
