801f10a0b41ea5ec1faa4e5762f2cbbbe30c03e7
Hardway Hou <hardway.hou@gmail.com>
2021-03-04 10:33:51 +0800

    bugfix: follow redirection correctly

--- rpc/client.php
+++ rpc/client.php
@@ -1,7 +1,7 @@
 <?php
 namespace Clue\RPC;
 include_once __DIR__."/common.php";
-
+include_once dirname(__DIR__)."/url.php";
 /**
  * Example:
  *
@@ -25,28 +25,28 @@ include_once __DIR__."/common.php";
  *
  */
 class Client{
-	use \Clue\Logger\LoggerTrait;
-	use \Clue\Traits\Bookkeeper;
+    use \Clue\Logger\LoggerTrait;
+    use \Clue\Traits\Bookkeeper;
 
-	/**
-	 * @param $endpoint	远程调用端的URL，格式 https://127.0.0.1/api/EchoService
-	 * @param $options 	选项，比如是否加密会话，通讯格式为php或者json
-	 */
-	function __construct($endpoint, $options=array()){
-		$this->endpoint=$endpoint;
-		$this->debug=@$options['debug']===true;
+    /**
+     * @param $endpoint 远程调用端的URL，格式 https://127.0.0.1/api/EchoService
+     * @param $options  选项，比如是否加密会话，通讯格式为php或者json
+     */
+    function __construct($endpoint, $options=array()){
+        $this->endpoint=$endpoint;
+        $this->debug=@$options['debug']===true;
 
-		$this->client=@$options['client'];		// 客户ID和令牌，用于身份认证
-		$this->token=@$options['token'];
-		$this->secret=@$options['secret'];		// 预共享密钥，用于加密通信数据
-		$this->timeout=@$options['timeout'] ?: 30;
-		$this->proxy=@$options['proxy'];
+        $this->client=@$options['client'];      // 客户ID和令牌，用于身份认证
+        $this->token=@$options['token'];
+        $this->secret=@$options['secret'];      // 预共享密钥，用于加密通信数据
+        $this->timeout=@$options['timeout'] ?: 30;
+        $this->proxy=@$options['proxy'];
         $this->compression=@$options['compression'];
 
-		$this->cache_dir=null;
-	}
+        $this->cache_dir=null;
+    }
 
-	function enable_cache($cache_dir, $cache_ttl=3600){
+    function enable_cache($cache_dir, $cache_ttl=3600){
         if(!is_dir($cache_dir)){
             mkdir($cache_dir, 0775, true);
         }
@@ -54,45 +54,51 @@ class Client{
         $this->cache_ttl=$cache_ttl;
     }
 
-	function disable_cache(){
-		$this->cache_dir=null;
-		$this->cache_ttl=0;
-	}
+    function disable_cache(){
+        $this->cache_dir=null;
+        $this->cache_ttl=0;
+    }
 
-	function __call($name, $arguments){
-		$this->info("[RPC] Connecting to $this->endpoint");
+    function debug_log($message){
+        if($this->debug){
+            error_log($message);
+        }
+    }
 
-		$payload=array('method'=>$name, 'params'=>$arguments);
-		if($this->client){
-			$payload['client']=$this->client;
-			$payload['token']=$this->token;
-		}
+    function __call($name, $arguments){
+        $this->info("[RPC] Connecting to $this->endpoint");
 
-		// For Bookkeeping
-		$record=[
-			'call_time'=>gmdate("Y-m-d H:i:s"),
-			'type'=>'out',
-			'endpoint'=>$this->endpoint,
-			'client'=>$this->client,
-			'method'=>$name,
-			'request'=>json_encode($arguments)
-		];
+        $payload=array('method'=>$name, 'params'=>$arguments);
+        if($this->client){
+            $payload['client']=$this->client;
+            $payload['token']=$this->token;
+        }
 
-		if($this->debug) error_log("[RPC] Payload: ".json_encode($payload));
+        // For Bookkeeping
+        $record=[
+            'call_time'=>gmdate("Y-m-d H:i:s"),
+            'type'=>'out',
+            'endpoint'=>$this->endpoint,
+            'client'=>$this->client,
+            'method'=>$name,
+            'request'=>json_encode($arguments)
+        ];
 
-		$payload=json_encode($payload);
+        $this->debug_log("[RPC] Payload: ".json_encode($payload));
 
-		// 检查cache是否命中
-		if($this->cache_dir){
-			$cache_file="$this->cache_dir/".md5($this->endpoint.$this->client.$this->token.$payload);
+        $payload=json_encode($payload);
 
-            if($this->debug) error_log("[RPC] Checking Cache: $cache_file");
+        // 检查cache是否命中
+        if($this->cache_dir){
+            $cache_file="$this->cache_dir/".md5($this->endpoint.$this->client.$this->token.$payload);
 
-			if(is_file($cache_file) && time()-filemtime($cache_file)<$this->cache_ttl){
-                if($this->debug) error_log("[RPC] Using Cache: $cache_file");
-				return json_decode(file_get_contents($cache_file), true);
-			}
-		}
+            $this->debug_log("[RPC] Checking Cache: $cache_file");
+
+            if(is_file($cache_file) && time()-filemtime($cache_file)<$this->cache_ttl){
+                $this->debug_log("[RPC] Using Cache: $cache_file");
+                return json_decode(file_get_contents($cache_file), true);
+            }
+        }
 
         if($this->compression){
             $payload=gzencode($payload);
@@ -102,98 +108,96 @@ class Client{
             $payload=clue_rpc_encrypt($payload, $this->secret);
         }
 
-		$c=curl_init();
+        $c=curl_init();
 
-		curl_setopt($c, CURLOPT_POST, 1);
-		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-		// 使用curl follow不会重新post
-		// curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($c, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($c, CURLOPT_POST, 1);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+        // 使用curl follow不会重新post
+        // curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($c, CURLOPT_POSTFIELDS, $payload);
 
-		// RPC不能超过5秒
-		curl_setopt($c, CURLOPT_CONNECTTIMEOUT, $this->timeout);
-		curl_setopt($c, CURLOPT_TIMEOUT, $this->timeout);
+        // RPC不能超过5秒
+        curl_setopt($c, CURLOPT_CONNECTTIMEOUT, $this->timeout);
+        curl_setopt($c, CURLOPT_TIMEOUT, $this->timeout);
 
-		// No SSL Verification
-		curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+        // No SSL Verification
+        curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
 
-		// Proxy
-		if($this->proxy){
-			if(preg_match('/^sock[45s]?:\/\/([a-z0-9\-_\.]+):(\d+)$/i', $this->proxy, $m)){
-				list($_, $host, $port)=$m;
-				curl_setopt($c, CURLOPT_PROXY, $host);
-				curl_setopt($c, CURLOPT_PROXYPORT, $port);
+        // Proxy
+        if($this->proxy){
+            if(preg_match('/^sock[45s]?:\/\/([a-z0-9\-_\.]+):(\d+)$/i', $this->proxy, $m)){
+                list($_, $host, $port)=$m;
+                curl_setopt($c, CURLOPT_PROXY, $host);
+                curl_setopt($c, CURLOPT_PROXYPORT, $port);
 
-				if(!defined('CURLPROXY_SOCKS5_HOSTNAME')) define('CURLPROXY_SOCKS5_HOSTNAME', 7);
-				curl_setopt($c, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5_HOSTNAME);
-				// curl_setopt($c, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-			}
-			elseif(preg_match('/^(http:\/\/)?([a-z0-9\-_\.]+):(\d+)/i', $this->proxy, $m)){
-				list($_, $scheme, $host, $port)=$m;
+                if(!defined('CURLPROXY_SOCKS5_HOSTNAME')) define('CURLPROXY_SOCKS5_HOSTNAME', 7);
+                curl_setopt($c, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5_HOSTNAME);
+                // curl_setopt($c, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+            }
+            elseif(preg_match('/^(http:\/\/)?([a-z0-9\-_\.]+):(\d+)/i', $this->proxy, $m)){
+                list($_, $scheme, $host, $port)=$m;
 
-				curl_setopt($c, CURLOPT_PROXY, $host);
-				curl_setopt($c, CURLOPT_PROXYPORT, $port);
-			}
-		}
+                curl_setopt($c, CURLOPT_PROXY, $host);
+                curl_setopt($c, CURLOPT_PROXYPORT, $port);
+            }
+        }
 
-		// curl_setopt($c, CURLOPT_HTTPHEADER, array("Expect:"));
-		curl_setopt($c, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+        // curl_setopt($c, CURLOPT_HTTPHEADER, array("Expect:"));
+        curl_setopt($c, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
 
-		$redirect=3;
+        $redirect=3;
 redirect:
-		curl_setopt($c, CURLOPT_URL, $this->endpoint);
-		$response=curl_exec($c);
-		$header = curl_getinfo($c);
+        curl_setopt($c, CURLOPT_URL, $this->endpoint);
+        $response=curl_exec($c);
+        $header = curl_getinfo($c);
 
-		if(curl_errno($c)){
-			$err=sprintf("NETWORK %d: %s", curl_errno($c), curl_error($c));
-			$this->bookkeep($record+['status'=>503, 'response'=>$err]);
-			throw new \Exception($err);
-		}
+        if(curl_errno($c)){
+            $err=sprintf("NETWORK %d: %s", curl_errno($c), curl_error($c));
+            $this->bookkeep($record+['status'=>503, 'response'=>$err]);
+            throw new \Exception($err);
+        }
 
-		if($header['http_code']>=400){
-			$this->bookkeep($record+['status'=>$header['http_code'], 'response'=>$response]);
+        if($header['http_code']>=400){
+            $this->bookkeep($record+['status'=>$header['http_code'], 'response'=>$response]);
 
-			if($header['http_code']==500){
-				if(preg_match('/^\d+ /', $response)){
-					list($code, $error)=explode(" ", $response, 2);
-					throw new \Exception($error, $code);
-				}
-				else{
-					throw new \Exception($response);
-				}
-			}
-			else
-				throw new \Exception("HTTP {$header['http_code']} $response");
-		}
-		elseif(preg_match('/3\d\d/', $header['http_code'])){
-			// 自动Follow新的URL
-			$this->endpoint=$header['redirect_url'];
-			$redirect--;
-			if($redirect>0) goto redirect;
-		}
+            if($header['http_code']==500){
+                if(preg_match('/^\d+ /', $response)){
+                    list($code, $error)=explode(" ", $response, 2);
+                    throw new \Exception($error, $code);
+                }
+                else{
+                    throw new \Exception($response);
+                }
+            }
+            else
+                throw new \Exception("HTTP {$header['http_code']} $response");
+        }
+        elseif(preg_match('/3\d\d/', $header['http_code'])){
+            // 自动Follow新的URL
+            $this->endpoint=url_follow($header['redirect_url'], $this->endpoint);
+            $this->debug_log("[RPC] Redirecting: ".$this->endpoint);
 
-		curl_close($c);
+            $redirect--;
+            if($redirect>0) goto redirect;
+        }
 
-		if($this->secret){
-			if($this->debug){
-				error_log(sprintf("[RPC] ENCRYPTED RESPONSE:\n====================\n%s\n\n", $response));
-			}
-			$response=clue_rpc_decrypt($response, $this->secret);
-		}
+        curl_close($c);
 
-		$this->bookkeep($record+['status'=>200, 'response'=>$response]);
+        if($this->secret){
+            $this->debug_log(sprintf("[RPC] ENCRYPTED RESPONSE:\n====================\n%s\n\n", $response));
+            $response=clue_rpc_decrypt($response, $this->secret);
+        }
 
-		if($this->debug){
-			error_log(sprintf("[RPC] RESPONSE:\n====================\n%s\n\n", $response));
-		}
+        $this->bookkeep($record+['status'=>200, 'response'=>$response]);
 
-		if($this->cache_dir){
-            if($this->debug) error_log("[RPC] Writing Cache: $cache_file");
-			file_put_contents($cache_file, $response);
-		}
+        $this->debug_log(sprintf("[RPC] RESPONSE:\n====================\n%s\n\n", $response));
 
-		return json_decode($response, true);
-	}
+        if($this->cache_dir){
+            $this->debug_log("[RPC] Writing Cache: $cache_file");
+            file_put_contents($cache_file, $response);
+        }
+
+        return json_decode($response, true);
+    }
 }
