c17caf5dc39bc726f27be0ebc98614d480d5cfb7
jiaocui <jiaocui@TTdeMac-mini.local>
2026-06-12 21:41:55 +0800

    rpc: remove rpc-encdec.test.php (now in rpc.test.php), add extension guard
    - Delete tests/rpc-encdec.test.php (encdec test duplicated in rpc.test.php)
    - rpc/common.php: check order now mcrypt → openssl → throw RuntimeException
      (was openssl-only with silent mcrypt fallback)

--- rpc/common.php
+++ rpc/common.php
@@ -1,33 +1,45 @@
 <?php
 function clue_rpc_encrypt($str, $secret=RPC_SECRET){
-    $block=8; // mcrypt_get_block_size('des', 'ecb');
+    $block=8;
     $pad = $block - (strlen($str) % $block);
     $str .= str_repeat(chr($pad), $pad);
 
     $l = strlen($secret);
     if ($l < 16) $secret = str_repeat($secret, ceil(16/$l));
 
-    if(extension_loaded("openssl")){
+    if (extension_loaded('mcrypt')) {
+        return base64_encode(
+            mcrypt_encrypt(MCRYPT_BLOWFISH, $secret, $str, MCRYPT_MODE_ECB)
+        );
+    }
+
+    if (extension_loaded('openssl')) {
         return base64_encode(
             openssl_encrypt($str, 'BF-ECB', $secret, OPENSSL_RAW_DATA | OPENSSL_NO_PADDING)
         );
     }
-    else{
-        return base64_encode(mcrypt_encrypt(MCRYPT_BLOWFISH, $secret, $str, MCRYPT_MODE_ECB));
-    }
+
+    throw new RuntimeException(
+        'clue_rpc_encrypt requires mcrypt or openssl extension'
+    );
 }
 
 function clue_rpc_decrypt($str, $secret=RPC_SECRET){
     $l = strlen($secret);
     if ($l < 16) $secret = str_repeat($secret, ceil(16/$l));
 
-    if(extension_loaded("openssl")){
+    if (extension_loaded('mcrypt')) {
+        $str = mcrypt_decrypt(
+            MCRYPT_BLOWFISH, $secret, base64_decode($str), MCRYPT_MODE_ECB
+        );
+    } elseif (extension_loaded('openssl')) {
         $str = openssl_decrypt(
             base64_decode($str), 'BF-ECB', $secret, OPENSSL_RAW_DATA | OPENSSL_NO_PADDING
         );
-    }
-    else{
-        $str = mcrypt_decrypt(MCRYPT_BLOWFISH, $secret, base64_decode($str), MCRYPT_MODE_ECB);
+    } else {
+        throw new RuntimeException(
+            'clue_rpc_decrypt requires mcrypt or openssl extension'
+        );
     }
 
     $pad = ord($str[($len = strlen($str)) - 1]);
